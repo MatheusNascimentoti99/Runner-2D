fc54750550c4a8d26cbd25992d81fcc7dab40909
Matheus Nascimento <matheunascimentoti99@gmail.com>
2020-08-29 22:57:15 -0300

    Add prefab of wall

---
+++ Assets/Scripts/Mola.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mola : MonoBehaviour
+{
+    public float impulseForce;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody2D rigidbody2 = other.GetComponent<Rigidbody2D>();
+            rigidbody2.velocity = Vector2.zero;
+            rigidbody2.AddForce(Vector2.up * impulseForce, ForceMode2D.Impulse);
+        }
+    }
+}
